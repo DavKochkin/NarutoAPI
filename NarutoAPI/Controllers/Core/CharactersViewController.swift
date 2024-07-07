4a56a26b1fb6aac4749f7c840d94f5c46e545b23
David Kochkin <kochkin.david@gmail.com>
2024-07-07 18:37:21 +0300

    Configure TabBar

--- NarutoAPI/Controllers/Core/CharactersViewController.swift
+++ NarutoAPI/Controllers/Core/CharactersViewController.swift
@@ -10,20 +10,9 @@ import UIKit
 class CharactersViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Characters"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- NarutoAPI/Controllers/Core/TabViewController.swift
+++ NarutoAPI/Controllers/Core/TabViewController.swift
@@ -11,10 +11,47 @@ class TabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        setUpTabs()
     }
 
-
+    func setUpTabs() {
+        let characterVC = CharactersViewController()
+        let clanVC      = ClanViewController()
+        let villagesVC  = VillagesViewController()
+        let settingsVC  = SettingsViewController()
+        
+        characterVC.navigationItem.largeTitleDisplayMode = .automatic
+        clanVC.navigationItem.largeTitleDisplayMode      = .automatic
+        villagesVC.navigationItem.largeTitleDisplayMode  = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode  = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: characterVC)
+        let nav2 = UINavigationController(rootViewController: clanVC)
+        let nav3 = UINavigationController(rootViewController: villagesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Character",
+                                       image: UIImage(systemName: "person"),
+                                       tag: 1)
+        
+        nav2.tabBarItem = UITabBarItem(title: "Clan",
+                                       image: UIImage(systemName: "person.3"),
+                                       tag: 2)
+        
+        nav3.tabBarItem = UITabBarItem(title: "Villages",
+                                       image: UIImage(systemName: "house"),
+                                       tag: 3)
+        
+        nav4.tabBarItem = UITabBarItem(title: "Settings",
+                                       image: UIImage(systemName: "gear"),
+                                       tag: 4)
+        
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
+    }
 }
 
